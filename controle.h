93804a0e643a90320cc7d4e689a5786993d647d9
Paulo Dezingrini <paulo.dezingrini@ufv.br>
2022-08-05 19:40:49 -0300

    Adicionando as funções de controle e exibição da main

---
+++ controle.h
@@ -0,0 +1,105 @@
+#ifndef CONTROLE_H
+#define CONTROLE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "RSA.h"
+
+using namespace std;
+
+/*
+    Funções responsáveis por controlar e gerenciar o funcionamento da main
+    Funções de exibição:
+*/
+
+void exibirChaves();
+void exibirMensagemCriptografada(vector<long long> mensagemCriptografada);
+void exibirMensagemDescriptografada(string mensagem);
+
+/*
+    Funções de controle
+*/
+
+RSA gerarChaves();
+void criptografarMensagem(string mensagem);
+void criptografarMensagem(long long n, long long e, string mensagem);
+void assinarMensagem(string mensagem);
+void execucaoCompleta(string mensagem);
+
+/*
+    Implementação das Funções de exibição
+*/
+
+void exibirChaves(RSA rsa)
+{
+    cout
+        << "\t\tChaves públicas: ( " << rsa.getChavePublica_n() << " , " << rsa.getChavePublica_e() << " )" << endl;
+    cout << "\t\tChave privada: " << rsa.getChavePrivada() << endl;
+}
+
+void exibirMensagemCriptografada(vector<long long> mensagemCriptografada)
+{
+    cout << "\t\tMensagem Criptografada: ";
+    for (int i = 0; i < mensagemCriptografada.size(); i++)
+        cout << mensagemCriptografada[i] << " ";
+    cout << endl;
+}
+
+void exibirMensagemDescriptografada(string mensagem)
+{
+    cout << "\t\tMensagem Descriptografada: " << mensagem << endl;
+}
+
+/*
+    Implementação das Funções de controle
+*/
+
+RSA gerarChaves()
+{
+    RSA rsa;
+    rsa.gerarChaves();
+    exibirChaves(rsa);
+    return rsa;
+}
+
+void criptografarMensagem(string mensagem)
+{
+    RSA rsa = gerarChaves();
+    vector<long long> mensagemCriptografada = rsa.criptografar(mensagem);
+    exibirMensagemCriptografada(mensagemCriptografada);
+}
+
+void criptografarMensagem(long long n, long long e, string mensagem)
+{
+    RSA rsa(n, e);
+    vector<long long> mensagemCriptografada = rsa.criptografar(mensagem);
+    exibirMensagemCriptografada(mensagemCriptografada);
+}
+
+void execucaoCompleta(string mensagem)
+{
+    RSA rsa = gerarChaves();
+    vector<long long> mensagemCriptografada = rsa.criptografar(mensagem);
+    exibirMensagemCriptografada(mensagemCriptografada);
+    string mensagemDescriptografada = rsa.descriptografar(mensagemCriptografada);
+    exibirMensagemDescriptografada(mensagemDescriptografada);
+}
+
+void assinarMensagem(string mensagem)
+{
+    RSA rsa = gerarChaves();
+    vector<long long> mensagemAssinada = rsa.gerarAssinatura(mensagem);
+    exibirMensagemCriptografada(mensagemAssinada);
+}
+
+void execucaoCompletaAssinatura(string mensagem)
+{
+    RSA rsa = gerarChaves();
+    vector<long long> mensagemAssinada = rsa.gerarAssinatura(mensagem);
+    exibirMensagemCriptografada(mensagemAssinada);
+    string mensagemDescriptografada = rsa.descriptografarAssinatura(mensagemAssinada);
+    exibirMensagemDescriptografada(mensagemDescriptografada);
+}
+
+#endif
